047133d0bb9f54748643c904b18ece3cbd75ff7f
jared <jared@Blade>
2022-05-11 13:55:49 +1200

    DatabaseInitializer completed, changes to DBManager

--- src/Model/Data/DBManager.java
+++ src/Model/Data/DBManager.java
@@ -1,9 +1,19 @@
 package Model.Data;
 
 // @author Jared Scholz
+import Model.Entity.Item;
+import Model.Map.Enemy;
+import Model.Map.Merchant;
+import Model.Map.PassiveEvent;
+import Model.Map.Scene;
+import Model.Map.Trap;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBManager {
 
@@ -12,8 +22,22 @@ public class DBManager {
     private static final String URL = "jdbc:derby:GameData_Ebd; create=true";
     private Connection connection;
 
+    private final List<Item> allItems;
+    private final List<Enemy> allEnemies;
+    private final List<Merchant> allMerchants;
+    private final List<PassiveEvent> allPassiveEvents;
+    private final List<Trap> allTraps;
+    private final List<Scene> allScenes;
+
     public DBManager() {
         establishConnection();
+
+        allItems = new ArrayList<>();
+        allEnemies = new ArrayList<>();
+        allMerchants = new ArrayList<>();
+        allPassiveEvents = new ArrayList<>();
+        allTraps = new ArrayList<>();
+        allScenes = new ArrayList<>();
     }
 
     private void establishConnection() {
@@ -27,5 +51,43 @@ public class DBManager {
 
     public Connection getConnection() {
         return connection;
+    }
+
+    private void loadArmor() {
+        try {
+            Statement statement = connection.createStatement();
+
+            ResultSet rs = statement.executeQuery("SELECT * FROM ARMOR");
+            while (rs.next()) {
+                // TODO: add armor to items list
+            }
+        } catch (SQLException ex) {
+            System.out.println("ERROR GETTING ITEMS");
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public List<Item> getAllItems() {
+        return allItems;
+    }
+
+    public List<Enemy> getAllEnemies() {
+        return allEnemies;
+    }
+
+    public List<Merchant> getAllMerchants() {
+        return allMerchants;
+    }
+
+    public List<PassiveEvent> getAllPassiveEvents() {
+        return allPassiveEvents;
+    }
+
+    public List<Trap> getAllTraps() {
+        return allTraps;
+    }
+
+    public List<Scene> getAllScenes() {
+        return allScenes;
     }
 }
